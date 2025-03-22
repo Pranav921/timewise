532056c5cbd47bbfa37ee9900412fa9ff9a79819
Rohun Gargya <rohun.gargya@gmail.com>
2025-03-22 19:28:35 -0400

    Started database creation

--- backend/src/web_scrapers/degrees_web_scraper.py
+++ backend/src/web_scrapers/degrees_web_scraper.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from pprint import pprint
+from data_templates.certificate_template import Certificate
+from data_templates.major_template import Major
+from data_templates.minor_template import Minor
 
 # Get links on all majors, minors, and certificates offered at UF
 def get_all_data(catalog_url):
@@ -43,17 +46,18 @@ def split_dict(input_dict):
 # Sometimes required courses don't show up
 # Possibly more issues as I have checked each individiual certificate
 def get_certificate_data(dict):
+    all_certificates = []
+    max_val = 0
     base_url = "https://catalog.ufl.edu"
-    for key, value in dict.items():
+    counter = 0
+    for name, value in dict.items():
         url = f"{base_url}{value}"
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         list_items = soup.find_all('li')
         p_elements = soup.find_all('p')
-        print("Certificate Name: " + key)
         print("Certificate URL: " + url)
         description = ' '.join([p.get_text() for p in soup.find_all('p') if 'certificate' in p.get_text().lower()])
-        print("Certificate Description: " + description)
         prerequisite_courses = []
         required_courses = []
         # Extract courses from Prerequisite Courses
@@ -73,32 +77,35 @@ def get_certificate_data(dict):
             if required_table:
                 required_links = required_table.find_all('a', class_='bubblelink code')
                 required_courses = [link.text.strip().replace('\xa0', '') for link in required_links]
-        print("Prerequisite Courses:", prerequisite_courses)
-        print("Required Courses:", required_courses)
-
+        credits = -1
+        college_name = "-1"
         # Get all the College Names
         for li in list_items:
             if li.find('strong') and 'College:' in li.find('strong').text:
                 college_name = li.find('a').text.strip()
-                print("College Name: " + college_name)
             if li.find('strong') and 'Credits:' in li.find('strong').text:
                 credits_text = li.text.strip()
                 credits = re.search(r'\d+', credits_text.lstrip()).group()
-                print("Credits: " + credits)
-        print("#################################################")
+        # scrape the admissions criteria (ex: https://catalog.ufl.edu/UGRD/colleges-schools/UGENG/ENG_UCT12/)
+        # Add the electives
+        certificate = Certificate(name, credits, description, college_name, prerequisite_courses, "Undergraduate", required_courses, [])
+        all_certificates.append(certificate)
+        if len(description) > max_val:
+            max_val = len(description)
+        counter += 1
+    return all_certificates
 
 def get_minor_data(dict):
+    all_minors = []
     base_url = "https://catalog.ufl.edu"
-    for key, value in dict.items():
+    for name, value in dict.items():
         url = f"{base_url}{value}"
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
+        print("Minor URL: " + url)
         list_items = soup.find_all('li')
         p_elements = soup.find_all('p')
-        print("Minor Name: " + key)
-        print("Minor URL: " + url)
         description = ' '.join([p.get_text() for p in soup.find_all('p') if 'minor' in p.get_text().lower()])
-        print("Minor Description: " + description)
         prerequisite_courses = []
         required_courses = []
         # Extract courses from Prerequisite Courses
@@ -122,16 +129,13 @@ def get_minor_data(dict):
                 or_cols = soup.find_all('td', class_='codecol orclass')
                 for course in or_cols:
                     course_code = course.find('a').text
-                    print(course_code)
-        print("Prerequisite Courses:", prerequisite_courses)
-        print("Required Courses:", required_courses)
-
+        college_name = "-1"
+        credits = -1
         # Get all the College Names
         for li in list_items:
             if li.find('strong') and 'College:' in li.find('strong').text:
                 college_name = li.text.strip()
                 college_name = college_name.replace('College: ', '')
-                print("College Name: " + college_name)
             if li.find('strong') and 'Credits:' in li.find('strong').text:
                 credits_text = li.text.strip()
                 credits = re.search(r'\d+', credits_text.lstrip())
@@ -139,24 +143,25 @@ def get_minor_data(dict):
                     credits = credits.group()
                 else:
                     credits = -1
-                print("Credits: " + str(credits))
-        print("#################################################")
+        # Add non-eligible majors (ex:https://catalog.ufl.edu/UGRD/colleges-schools/UGENG/CIE_UMN/)
+        minor = Minor(name, credits, description, college_name, prerequisite_courses, "", "Undergraduate", required_courses, [])
+        all_minors.append(minor)
+    return all_minors
 
 def get_major_data(dict):
+    all_majors = []
     base_url = "https://catalog.ufl.edu"
-    for key, value in dict.items():
+    for name, value in dict.items():
         url = f"{base_url}{value}"
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         list_items = soup.find_all('li')
         p_elements = soup.find_all('p')
-        print("Major Name: " + key)
         print("Major URL: " + url)
         intro_text_div = soup.find('div', id='intro-text')
         description = str(intro_text_div.find_all('p')[0])
         description = description.replace('<p>', '')
         description = description.replace('</p>', '')
-        print("Major Description: " + str(description))
         prerequisite_courses = []
         required_courses = []
         # Extract courses from Prerequisite Courses
@@ -176,43 +181,19 @@ def get_major_data(dict):
             if required_table:
                 required_links = required_table.find_all('a', class_='bubblelink code')
                 required_courses = [link.text.strip().replace('\xa0', '') for link in required_links]
-        print("Prerequisite Courses:", prerequisite_courses)
-        print("Required Courses:", required_courses)
-
+        credits = -1
+        college_name = "-1"
         # Get all the College Names
         for li in list_items:
             if li.find('strong') and 'College:' in li.find('strong').text:
                 college_name = li.text.strip()
                 college_name = college_name.replace('College: ', '')
-                print("College Name: " + college_name)
             if li.find('strong') and 'Credits for Degree:' in li.find('strong').text:
                 credits_text = li.text.strip()
-                credits = re.search(r'\d+', credits_text.lstrip()).group()
-                print("Credits: " + credits)
-        print("#################################################")
-
-def main():
-    catalog_url = "https://catalog.ufl.edu/UGRD/programs/#filter=.filter_24"
-    all_data = get_all_data(catalog_url) # Get links for all majors, minors, and certificates
-    split_data = split_dict(all_data)
-    minors = split_data[0]
-    certificates = split_data[1]
-    majors = split_data[2]
-    get_minor_data(minors)
-    get_certificate_data(certificates)
-    get_major_data(majors)
-    # print("Minors:")
-    # pprint(minors)
-    # print("#####################################################################################"*3)
-    # print("#####################################################################################" * 3)
-    # print("#####################################################################################" * 3)
-    # print("Certificates:")
-    # pprint(certificates)
-    # print("#####################################################################################"*3)
-    # print("#####################################################################################" * 3)
-    # print("#####################################################################################" * 3)
-    # print("Majors:")
-    # pprint(major)
-
-if __name__ == "__main__":
-    main()
+                credits = re.search(r'\d+', credits_text.lstrip())
+                if(credits):
+                    credits = credits.group()
+        major = Major(name, credits, description, college_name, prerequisite_courses, "Undergraduate",
+                      required_courses, [], "Elective Description Needed")
+        all_majors.append(major)
+    return all_majors
